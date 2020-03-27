e3f95cb44380753649f6a4d79f37a49a5d004ccb
Karan Negi <52632768+karan-negi-au7@users.noreply.github.com>
2020-03-28 02:54:28 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,18 @@
+# MAZE SOLVER 
+
+## _Python project to solve maze contained in text file_.
+
+Program takes the input and output file name as command-line arguments.
+Using the square matrix present in the input file it should generate a path to reach the end of the maze and put it in the output file.
+***
+
+**Technologies Involve**
+>Project is created with `Python3.8.2` using [argparse](https://docs.python.org/3/library/argparse.html) module
+
+
+**Installation**
+> To run this project, download and install `YSetup.exe` locally on your system 
+
+**How to run**
+>Installing setup leads to `Maze Solver` file, which contains a `input.txt` file. In this file you need to put matrix of nxn size and have to double click on `mazesolver.bat` if you were using windows or `mazesolver.sh` if you were using linux to get the output matrix(which comes up in the `path.txt` file).
+>
---
+++ main.py
@@ -0,0 +1,106 @@
+import argparse
+import datetime
+
+#function to solve the maze using backtracking
+def solveMaze(r, c):
+    #if destination is reached
+    if (r==SIZE-1) and (c==SIZE-1):
+        solution[r][c] = 1;
+        return True
+    #checking if we can visit in the cell or not    
+    if r>=0 and c>=0 and r<SIZE and c<SIZE and solution[r][c] == 0 and maze[r][c] == 1:
+        solution[r][c] = 1 #visiting the cell
+        if solveMaze(r+1, c): #going down
+            return True
+        if solveMaze(r, c+1): #going right
+            return True
+        if solveMaze(r-1, c): #going up
+            return True
+        if solveMaze(r, c-1): #going left
+            return True
+        #backtracking    
+        solution[r][c] = 0;
+        return False;
+    return False;
+
+def printSolution( sol ):
+    f1.write("***\n" + "Following the path " + moveCheck(sol) + " leads to the destination:\n\n")
+    for i in sol:
+        for j in i:
+            f1.write(" " + str(j) + " ")
+        f1.write('\n') 
+    now = datetime.datetime.now()
+    f1.write("\nExecuted at: " + now.strftime("%Y-%m-%d*%H:%M:%S\n" + "***\n\n"))
+
+#function to get the movements need to be done to reach the destination.
+def moveCheck(solvedMaze):
+    n = len(solvedMaze)
+    visited = [[False for _ in range(n)] for _ in range(n)]
+    arr = ""
+    i = j = 0
+    while i!=n-1 or  j!=n-1:
+        if j == n-1:
+            if solvedMaze[i+1][j] == 1 and visited[i+1][j] == False:
+                visited[i+1][j] = True
+                i = i+1
+                arr+="D"
+            elif solvedMaze[i][j-1] == 1 and visited[i][j-1] == False:
+                visited[i][j-1] = True
+                j = j-1
+                arr+="L"  
+        elif i == n-1:
+            if solvedMaze[i][j+1] == 1 and visited[i][j+1] == False:
+                visited[i][j+1] = True
+                j = j+1
+                arr+="R"  
+            elif solvedMaze[i-1][j] == 1 and visited[i-1][j] == False:
+                visited[i-1][j] = True
+                i = i-1
+                arr+="U"    
+        else:
+            if solvedMaze[i+1][j] == 1 and visited[i+1][j] == False:
+                visited[i+1][j] = True
+                i = i+1
+                arr+="D"
+            elif solvedMaze[i][j+1] == 1 and visited[i][j+1] == False:
+                visited[i][j+1] = True
+                j = j+1
+                arr+="R"  
+            elif solvedMaze[i-1][j] == 1 and visited[i-1][j] == False:
+                visited[i-1][j] = True
+                i = i-1
+                arr+="U"
+            elif solvedMaze[i][j-1] == 1 and visited[i][j-1] == False:
+                visited[i][j-1] = True
+                j = j-1
+                arr+="L"
+    return arr
+
+if __name__ == "__main__":	
+    maze = []
+    #taking file arguments from command line
+    parser = argparse.ArgumentParser()
+    parser.add_argument("ipFile", help="Input File")
+    parser.add_argument("opFile", help="Output File")
+    args = parser.parse_args()
+   
+    f = open(args.ipFile,'r')#input file
+    f1 = open(args.opFile,'a')#output file
+    
+    #generating maze from inputs
+    for data in f:
+        [l.strip('\n\r') for l in data]
+        maze.append([int(x) for x in data.split()])
+    
+    SIZE = len(maze)
+    #list to store the solution matrix
+    solution = [[0]*SIZE for _ in range(SIZE)]    
+    
+    '''MAZE-RUNNER'''
+    if(solveMaze(0,0)):
+        printSolution(solution)
+    else:
+        f1.write("***\n-1\n")
+        f1.write("No path available to reach destination.")
+        now = datetime.datetime.now()
+        f1.write("\nExecuted at: " + now.strftime("%Y-%m-%d*%H:%M:%S") + "\n***\n\n\n")
